d38b1a4b831701a7496a84db135c7e9f39a8ea13
mtt.gennari <mtt.gennari.work@gmail.com>
2020-04-28 19:40:00 +0200

    Visualizzazione pagina web grafana e inizio setStatus

--- ScaldoFragno/app/src/main/java/com/gennari/scaldofragno/MainActivity.java
+++ ScaldoFragno/app/src/main/java/com/gennari/scaldofragno/MainActivity.java
@@ -1,23 +1,86 @@
 package com.gennari.scaldofragno;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    RequestHandler requestHandler;
+    private RequestHandler requestHandler;
+    private Switch sCaldaia;
+    private SwipeRefreshLayout refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sCaldaia = findViewById(R.id.switchCaldaia);
+
         requestHandler = new RequestHandler(this, (TextView) findViewById(R.id.txtStatus),
                 (TextView) findViewById(R.id.txtTempIn),
-                (TextView) findViewById(R.id.txtLastRec));
+                (TextView) findViewById(R.id.txtLastRec),
+                sCaldaia);
         requestHandler.getStatoCaldaia();
         requestHandler.getTempIn();
+
+        refresh = findViewById(R.id.refresh);
+        refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestHandler.getStatoCaldaia();
+                requestHandler.getTempIn();
+                refresh.setRefreshing(false);
+            }
+        });
+
+        sCaldaia.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    requestHandler.setStatoCaldaia("Accesa");
+                    autoRefresh();
+                    return;
+                }else{
+                    requestHandler.setStatoCaldaia("Spenta");
+                    autoRefresh();
+                    return;
+                }
+            }
+        });
+
+        Button btStorico = findViewById(R.id.btStorico);
+        btStorico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://serverteknel.ddns.net:3000/d/M5L7K3qWk/fragno?orgId=1&refresh=5s&from=now-12h&to=now";
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                builder.setToolbarColor(getColor(R.color.colorPrimary));
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(MainActivity.this, Uri.parse(url));
+            }
+        });
+    }
+
+    private void autoRefresh(){
+        refresh.post(new Runnable() {
+            @Override
+            public void run() {
+                refresh.setRefreshing(true);
+                requestHandler.getStatoCaldaia();
+                requestHandler.getTempIn();
+                refresh.setRefreshing(false);
+            }
+        });
     }
 }
--- ScaldoFragno/app/src/main/java/com/gennari/scaldofragno/RequestHandler.java
+++ ScaldoFragno/app/src/main/java/com/gennari/scaldofragno/RequestHandler.java
@@ -1,8 +1,11 @@
 package com.gennari.scaldofragno;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -13,23 +16,29 @@ import com.android.volley.toolbox.Volley;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RequestHandler {
     private String statoCaldaia = "Sconosciuto";
     private String ultimaRegistazione;
     private String tempIn;
+    private String errore;
+
     private Context context;
+    private Switch sCaldaia;
 
     private TextView txtStatus, txtTempIn, txtLastRec;
 
     private RequestQueue queue;
 
-    public RequestHandler(Context context, TextView txtStatus, TextView txtTempIn, TextView txtLastRec){
+    public RequestHandler(Context context, TextView txtStatus, TextView txtTempIn, TextView txtLastRec, Switch sCaldaia){
         queue = Volley.newRequestQueue(context);
         this.context = context;
         this.txtStatus = txtStatus;
         this.txtTempIn = txtTempIn;
         this.txtLastRec =txtLastRec;
+        this.sCaldaia = sCaldaia;
     }
 
     public void getStatoCaldaia(){
@@ -40,6 +49,11 @@ public class RequestHandler {
             public void onResponse(String response) {
                 statoCaldaia = response;
                 txtStatus.setText(statoCaldaia);
+                sCaldaia.setVisibility(View.VISIBLE);
+                if(statoCaldaia.equals("Accesa"))
+                    sCaldaia.setChecked(true);
+                else
+                    sCaldaia.setChecked(false);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -75,6 +89,36 @@ public class RequestHandler {
         });
 
         queue.add(stringRequest);
+    }
+
+    public void setStatoCaldaia(final String status){
+        String url = "http://serverteknel.ddns.net:88/FragnoAPI/insert_status.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.equals("0"))
+                    response = errore;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                statoCaldaia = "ErroreConnessione";
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("status", status);
+                return params;
+            }
+        };
+
+        queue.add(stringRequest);
+    }
+
+    public String getStatoCaldaiaString(){
+        return statoCaldaia;
     }
 
 }
