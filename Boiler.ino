86eb8e7eaac5d136e7c0ac49b199b8230bbe563d
ictadmin <francesco.gennari@fcagroup.com>
2020-05-02 19:10:52 +0200

    test_commit_1

--- Boiler.ino
+++ Boiler.ino
@@ -3,7 +3,7 @@
 //jenkins
 //Definiamo una SoftwareSerial con i pin 8 ed 7 che devono essere collegati rispettivamente al TX ed RX del SIM800L
 SoftwareSerial SIM800L(8, 7);
-
+//
 float getVoltage(int);
 
 String lastLine="";
